e61bff09a30b48b2a5408b8deccb94d8474feee9
linhql1978 <linhql1978@gmail.com>
2020-07-09 23:38:26 +0700

    OK

--- extract_videos.py
+++ extract_videos.py
@@ -3,29 +3,28 @@ import glob
 import os
 import numpy as np
 
-listVideos=glob.glob('../demo2/videos/*.mp4')
-frames_dir= 'frames_dir'
+listVideos=glob.glob('videos/*.mp4') # Đọc các video trong folder videos
+frames_dir= 'frames_dir' # Khai báo tên folder để chứa các frames
 for video in listVideos:
-    print('video: ',video)
-    vidcap=cv2.VideoCapture(video)
+    vidcap=cv2.VideoCapture(video) # Xử lý video
     count=0
     success=True
     video_baseName=os.path.basename(video)
-    os.makedirs(os.path.join(frames_dir, video_baseName), exist_ok=True)
+    os.makedirs(os.path.join(frames_dir, video_baseName), exist_ok=True) # Tạo folder chứa các frames cho mỗi video
     while success:
-        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))
+        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000)) # Thiết lập đọc frame cho mỗi giây
         success,image=vidcap.read()
 
-        save_path=os.path.join(frames_dir, video_baseName, 'frame%d.jpg' % count)
+        save_path=os.path.join(frames_dir, video_baseName, 'frame%d.jpg' % count) # Tạo được dẫn lưu frame
 
-        image_last=cv2.imread(save_path+'frame{}.jpg'.format(count-1))
-        if np.array_equal(image,image_last):
+        image_last=cv2.imread(save_path+'frame{}.jpg'.format(count-1)) # Lấy ra frame được tách gần nhất trong video
+        if np.array_equal(image,image_last): # So sánh nếu 2 frame hiện tại và frame gần nhất giống nhau thì thoát vòng lặp
             break
 
-        cv2.imwrite(save_path,image)
+        cv2.imwrite(save_path,image) # Lưu frame vào đường dẫn đã được tạo
 
         print('{}.sec reading a new frame: {}'.format(count,True))
         count+=1
 
-    vidcap.release()
+    vidcap.release() # Giải phòng video
     print('Extract Success video: ', video_baseName)
--- find_video.py
+++ find_video.py
@@ -6,21 +6,20 @@ from scipy.spatial.distance import cdist
 np.seterr(divide='ignore', invalid='ignore')
 from numpy import linalg as LA
 import glob
-from sklearn.cluster import KMeans
 from scipy.spatial import distance
 
-#HOG
+# Hàm trích xuất đặc trưng sử dụng Histograms of Oriented Gradients
 def hog(img_gray, cell_size=8, block_size=2, bins=9):
     img = img_gray
     h, w = img.shape  # 128, 64
 
-    # gradient
+    # Tính đạo hàm image theo ox và oy
     xkernel = np.array([[-1, 0, 1]])
     ykernel = np.array([[-1], [0], [1]]) #
     dx = cv2.filter2D(img, cv2.CV_32F, xkernel)
     dy = cv2.filter2D(img, cv2.CV_32F, ykernel)
 
-    # histogram
+    # Tính biên độ và hướng của image dựa trên đạo hàm
     magnitude = np.sqrt(np.square(dx) + np.square(dy))
     orientation = np.arctan(np.divide(dy, dx + 0.00001))  # radian
     orientation = np.degrees(orientation)  # -90 -> 90
@@ -38,7 +37,7 @@ def hog(img_gray, cell_size=8, block_size=2, bins=9):
         pass
     pass
 
-    # normalization
+    # Chuẩn hóa và trả về vector đặc trưng của image
     redundant_cell = block_size - 1
     feature_tensor = np.zeros(
         [num_cell_y - redundant_cell, num_cell_x - redundant_cell, block_size * block_size * bins])
@@ -56,17 +55,16 @@ def hog(img_gray, cell_size=8, block_size=2, bins=9):
 
     return feature_tensor.flatten()
 
-# Kmeans
+# Khởi tạo ngẫu nhiên các centers
 def kmeans_init_centers(X, k):
-    # randomly pick k rows of X as initial centers
     return X[np.random.choice(X.shape[0], k, replace=False)]
 
+# Gán nhãn
 def kmeans_assign_labels(X, centers):
-    # calculate pairwise distances btw data and centers
     D = cdist(X, centers)
-    # return index of the closest center
     return np.argmin(D, axis = 1)
 
+# Cập nhật lại các centers
 def kmeans_update_centers(X, labels, K):
     centers = np.zeros((K, X.shape[1]))
     for k in range(K):
@@ -76,11 +74,13 @@ def kmeans_update_centers(X, labels, K):
         centers[k,:] = np.mean(Xk, axis = 0)
     return centers
 
+# Kiểm tra điều kiện dừng thuật toán Kmeans
 def has_converged(centers, new_centers):
     # return True if two sets of centers are the same
     return (set([tuple(a) for a in centers]) ==
         set([tuple(a) for a in new_centers]))
 
+# Dự đoán input sẽ thuộc cụm nào
 def predict(centers,input):
     distance_min=distance.euclidean(centers[0],input)
     index=0
@@ -93,6 +93,7 @@ def predict(centers,input):
         pass
     return index
 
+# Hàm Kmeans
 def kmeans(X, K):
     centers = [kmeans_init_centers(X, K)]
     labels = []
@@ -108,41 +109,41 @@ def kmeans(X, K):
     return (centers[-1], labels[-1], it)
 
 dim=64
-K=7
-list_videos=[]
-list_distance_videos=[]
+K=7 # Giá trị k để phân cụm
+list_videos=[] # Danh sách lưu tên các videos
+list_distance_videos=[] # Danh sách lưu khoảng cách ngắn nhất của input cho mỗi video
 
-img_test=cv2.imread('images_test/cycle/bycycle.jpeg',cv2.IMREAD_GRAYSCALE)
-img_test=cv2.resize(src=img_test,dsize=(dim,dim*2))
-vector_img_test=hog(img_test)
+img_test=cv2.imread('images_test/cycle/bycycle.jpeg',cv2.IMREAD_GRAYSCALE) # Đọc input ở chế độ ảnh xám
+img_test=cv2.resize(src=img_test,dsize=(dim,dim*2)) # Resize ảnh về (64,128) <=> (w,h)
+vector_img_test=hog(img_test) # Sử dụng HOG trích xuất đắc trưng của input
 
 videos_dir=glob.glob('../demo2/frames_dir/*.mp4')
 for video_dir in videos_dir:
     video_basename=os.path.basename(video_dir)
     frames=glob.glob('../demo2/frames_dir/'+video_basename+'/*.jpg')
-    X=[]
+    X=[] # Danh sách chứa các đặc trưng cho images trong video
     list_distance=[]
     for frame in frames:
         img=cv2.imread(frame,cv2.IMREAD_GRAYSCALE)
         img=cv2.resize(src=img,dsize=(dim,dim*2))
         vector_img=hog(img)
-        X.append(vector_img)
+        X.append(vector_img) # Lưu vector đặc trưng của image để phân cụm và tính toán
         pass
-    centers,labels,_=kmeans(np.array(X),K)
-    center_index=predict(centers,vector_img_test)
-    for i,feature_img in enumerate(X):
-        if(labels[i]==center_index):
-            list_distance.append(distance.euclidean(feature_img,vector_img_test))
+    centers,labels,_=kmeans(np.array(X),K) # Sử dụng Kmeans để phân cụm các frame của video với số cụm bằng K
+    center_index=predict(centers,vector_img_test) # Dự đoán input thuộc cụm nào
+    for i,feature_img in enumerate(X): # Duyệt tất cả các vector đặc trưng có trong X
+        if(labels[i]==center_index): # Kiểm tra xem vector đặc trưng có thuộc cụm đã tiên đoán cho input hay không?
+            list_distance.append(distance.euclidean(feature_img,vector_img_test)) # Tính toán bằng L2-norm và lưu lại
             pass
         pass
-    list_distance_videos.append(min(list_distance))
-    list_videos.append(video_basename)
+    list_distance_videos.append(min(list_distance)) # Tìm ra khoảng cách ngắn nhất của input tới frame trong video
+    list_videos.append(video_basename) # Lưu lại tên video
     pass
-print('list_videos: ',list_videos)
-print('list_distance_videos: ',list_distance_videos)
-dis_min=min(list_distance_videos)
+print('list_videos: ',list_videos) # In ra tên các video
+print('list_distance_videos: ',list_distance_videos) # In ra khoảng cách tương ứng
+dis_min=min(list_distance_videos) # Lấy khoảng cách nhỏ nhất của input tới frame trong các video
 for i in range(len(list_distance_videos)):
     if(dis_min==list_distance_videos[i]):
-        print('result video: ',list_videos[i])
+        print('result video: ',list_videos[i]) # In ra video
         pass
     pass
